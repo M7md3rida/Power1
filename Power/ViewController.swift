a89e7b4d7a663bcb31ddb5fa17b3e4e458d1a4fb
M7md3rida <41558729+M7md3rida@users.noreply.github.com>
2018-07-24 16:52:15 -0700

    Initial Commit

---
+++ Power/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Power
+//
+//  Created by mohamed on 7/24/18.
+//  Copyright © 2018 mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
